f58c401decd74932b30e1c46929c7f4442fbfabd
AdaN6 <a_disney_da@hotmail.com>
2023-01-08 19:50:34 +0100

    given the Crypto type and map the cryptos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 import './App.css';
 import axios from "axios";
 
+
+export type Crypto = {
+
+ath: number;
+atl: number;
+current_price: number;
+id: string;
+name: string;
+symbol: string;
+high_24h: number;
+low_24h: number;
+
+}
+
 function App() {
 
+  const [cryptos, setCryptos] = useState<Crypto[] | null>()
   useEffect(() => {
     const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     axios.get(url).then((response) => {
+      setCryptos(response.data)
       console.log(response.data)
     })
   }, [])
@@ -15,7 +31,9 @@ function App() {
 
   return (
     <div className="App">
-      Hiyaa
+      {cryptos ? cryptos.map((crypto)=> {
+        return <p>{crypto.name + ' $' + crypto.current_price}</p>
+      }) : null} 
     </div>
   );
 }
