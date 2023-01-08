844193f740de775eafd1b7aa26a1c7ae1624cac7
AdaN6 <a_disney_da@hotmail.com>
2023-01-08 19:39:18 +0100

    axios get data of coins market

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import {useEffect} from "react"
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+  useEffect(() => {
+    const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    axios.get(url).then((response) => {
+      console.log(response.data)
+    })
+  }, [])
+  
+  
+
   return (
     <div className="App">
-      
+      Hiyaa
     </div>
   );
 }
